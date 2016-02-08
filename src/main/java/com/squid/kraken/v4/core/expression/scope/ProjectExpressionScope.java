7a8a062ca39bf9349a0ca55ba4db7664b50e99a8
Serge Fantino <serge@squidsolutions.com>
2016-02-08 11:27:48 +0100

    T821: - support using a QueryExpresison to define the Domain

--- src/main/java/com/squid/kraken/v4/core/expression/scope/ProjectExpressionScope.java
+++ src/main/java/com/squid/kraken/v4/core/expression/scope/ProjectExpressionScope.java
@@ -23,53 +23,103 @@
  *******************************************************************************/
 package com.squid.kraken.v4.core.expression.scope;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import com.squid.core.database.domain.TableDomain;
 import com.squid.core.database.model.Table;
+import com.squid.core.domain.IDomain;
 import com.squid.core.expression.ExpressionAST;
+import com.squid.core.expression.parser.ExpressionParserImp;
+import com.squid.core.expression.parser.Token;
 import com.squid.core.expression.reference.TableReference;
 import com.squid.core.expression.scope.DefaultScope;
 import com.squid.core.expression.scope.ExpressionDiagnostic;
 import com.squid.core.expression.scope.ExpressionScope;
 import com.squid.core.expression.scope.IdentifierType;
 import com.squid.core.expression.scope.ScopeException;
-import com.squid.kraken.v4.core.analysis.universe.Physics;
+import com.squid.kraken.v4.api.core.AccessRightsUtils;
+import com.squid.kraken.v4.core.analysis.universe.Universe;
+import com.squid.kraken.v4.core.expression.reference.QueryExpression;
+import com.squid.kraken.v4.core.expression.reference.DomainReference;
+import com.squid.kraken.v4.core.model.domain.DomainDomain;
+import com.squid.kraken.v4.model.AccessRight.Role;
+import com.squid.kraken.v4.model.Domain;
+import com.squid.kraken.v4.model.ExpressionObject;
 
 public class ProjectExpressionScope extends DefaultScope {
 	
-	private Physics physics;
+	private Universe universe;
+	private Domain self = null;// if editing an existing Domain
+	private List<Domain> scope = null;
 
-	public ProjectExpressionScope(Physics physics) {
-		this.physics = physics;
+	public ProjectExpressionScope(Universe universe) {
+		this.universe = universe;
+	}
+
+	public ProjectExpressionScope(Universe universe, Domain self) {
+		this.universe = universe;
+		this.self = self;
+	}
+
+	public ProjectExpressionScope(Universe universe, Domain self, List<Domain> scope) {
+		this.universe = universe;
+		this.self = self;
+		this.scope  = scope;
+	}
+	
+	private List<Domain> getDomains() throws ScopeException {
+		if (this.scope!=null) {
+			return this.scope;
+		} else {
+			return universe.getDomains();
+		}
 	}
 
 	@Override
-	public ExpressionScope applyExpression(ExpressionAST first) throws ScopeException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public IdentifierType lookupIdentifierType(String image)
-			throws ScopeException {
-		// TODO Auto-generated method stub
-		return null;
+	public ExpressionScope applyExpression(ExpressionAST expression) throws ScopeException {
+		if (expression instanceof DomainReference) {
+			if (scope==null) {
+				return new QueryExpressionScope(universe, ((DomainReference)expression).getDomain());
+			} else {
+				return new QueryExpressionScope(universe, ((DomainReference)expression).getDomain(), Collections.<ExpressionObject<?>>emptyList());
+			}
+		} else {
+			throw new ScopeException("cannot compose");
+		}
 	}
 
 	@Override
 	public Object lookupObject(IdentifierType identifierType, String identifier)
 			throws ScopeException {
-		try {
-			Table table = physics.getTable(identifier);
-			if (table==null) {
+		//
+		// lookup for Domain
+		if (identifierType==null || identifierType==IdentifierType.DEFAULT) {
+			for (Domain domain : getDomains()) {
+				if (domain.getName().equals(identifier)) {
+					if (self==null || self!=domain) {
+						return domain;
+					}
+				}
+			}
+		}
+		//
+		//lookup for table
+		if (identifierType==null || identifierType==IdentifierType.DEFAULT || identifierType==IdentifierType.TABLE) {
+			try {
+				Table table = universe.getTable(identifier);
+				if (table==null) {
+					throw new ScopeException("cannot lookup the table '"+identifier+"'");
+				}
+				return table;
+			} catch (ExecutionException e) {
 				throw new ScopeException("cannot lookup the table '"+identifier+"'");
 			}
-			return table;
-		} catch (ExecutionException e) {
-			throw new ScopeException("cannot lookup the table '"+identifier+"'");
 		}
+		//
+		// else
+		return super.lookupObject(identifierType, identifier);
 	}
 
 	@Override
@@ -77,6 +127,8 @@ public class ProjectExpressionScope extends DefaultScope {
 			throws ScopeException {
 		if (reference instanceof Table) {
 			return new TableReference((Table)reference);
+		} else if (reference instanceof Domain) {
+			return new DomainReference(universe, (Domain)reference);
 		} else
 			return super.createReferringExpression(reference);
 	}
@@ -85,20 +137,66 @@ public class ProjectExpressionScope extends DefaultScope {
 	public void buildDefinitionList(List<Object> definitions) {
 		super.buildDefinitionList(definitions);
 		//
+		// list the domains visible to the user
 		try {
-            List<Table> tables = physics.getTables();
-            definitions.addAll(tables);
-        } catch (ExecutionException e) {
-            // ignore ?
-        }
+			// create the spaces here so we can exclude self if defined
+			for (Domain domain : getDomains()) {
+				if (this.self==null || this.self!=domain) {
+					definitions.add(domain);
+				}
+			}
+		} catch (ScopeException e1) {
+			// ignore
+		}
+		//
+		// list the tables only if the user has some super powers
+		if (AccessRightsUtils.getInstance().hasRole(universe.getContext(), universe.getProject(), Role.WRITE)) {
+			// this will prevent anyone to be able to lit the database tables
+			try {
+	            List<Table> tables = universe.getTables();
+	            definitions.addAll(tables);
+	        } catch (ExecutionException e) {
+	            // ignore
+	        }
+		}
 	}
 	
 	@Override
 	public ExpressionDiagnostic validateExpression(ExpressionAST expression) {
 		if (expression.getImageDomain().isInstanceOf(TableDomain.DOMAIN)) {
 			return ExpressionDiagnostic.IS_VALID;
+		} else if (expression.getImageDomain().isInstanceOf(DomainDomain.DOMAIN)) {
+				return ExpressionDiagnostic.IS_VALID;
+		} else if (expression.getImageDomain().isInstanceOf(IDomain.OBJECT)) {
+			// TODO: need to have a better appropriate domain
+			return ExpressionDiagnostic.IS_VALID;
 		} else {
-			return new ExpressionDiagnostic("The expression must be a valid table");
+			return new ExpressionDiagnostic("The expression must be a valid table or domain");
+		}
+	}
+	
+	@Override
+	public ExpressionAST createCompose(ExpressionAST first, ExpressionAST second, Token operator)
+			throws ScopeException {
+		if (operator.kind==ExpressionParserImp.FILTER) {
+			return createAnalysisExpression(first, operator).filter(second);
+		} else if (operator.kind==ExpressionParserImp.FACET) {
+			return createAnalysisExpression(first, operator).facet(second);
+		} else if (operator.kind==ExpressionParserImp.METRIC) {
+			return createAnalysisExpression(first, operator).metric (second);
+		} else {
+    		throw new ScopeException("composition operator '"+operator.image+"' is not supported in this scope");
+		}
+	}
+	
+	private QueryExpression createAnalysisExpression(ExpressionAST first, Token operator) throws ScopeException {
+		if (first instanceof DomainReference) {
+			DomainReference ref = (DomainReference)first;
+			return new QueryExpression(universe, ref.getDomain());
+		} else if (first instanceof QueryExpression) {
+			return (QueryExpression)first;
+		} else {
+			throw new ScopeException("cannot apply operator "+operator.image);
 		}
 	}
 
