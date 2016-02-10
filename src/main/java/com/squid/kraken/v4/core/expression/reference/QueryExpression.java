4f76600e74471b6e1bbff528919acf953ee7b35a
Serge Fantino <serge@squidsolutions.com>
2016-02-10 12:00:34 +0100

    T821: - refactoring QueryExpression:   - it's a root expression, not a LEAF   - the Domain is stored as a reference

--- src/main/java/com/squid/kraken/v4/core/expression/reference/QueryExpression.java
+++ src/main/java/com/squid/kraken/v4/core/expression/reference/QueryExpression.java
@@ -6,38 +6,32 @@ import java.util.List;
 import com.squid.core.domain.IDomain;
 import com.squid.core.domain.operators.ExtendedType;
 import com.squid.core.expression.ExpressionAST;
-import com.squid.core.expression.ExpressionLeaf;
 import com.squid.core.expression.scope.ScopeException;
 import com.squid.core.sql.render.SQLSkin;
-import com.squid.kraken.v4.core.analysis.universe.Universe;
-import com.squid.kraken.v4.core.model.domain.ProxyDomainDomain;
-import com.squid.kraken.v4.model.Domain;
 
 /**
  * The QueryExpression allows to define a Query (or view, or analysis) on a Domain.
  * It will be equivalent to defining a AnalysisJob, though it doesn't require to create a model object.
  * 
- * A QueryExpression can be used to define a new DOmain Subject, thus allowing to run complex computations
+ * A QueryExpression can be used to define a new Domain Subject, thus allowing to run complex computations
  * 
  * @author sergefantino
  *
  */
-public class QueryExpression implements ExpressionLeaf {
+public class QueryExpression implements ExpressionAST {
 	
-	private Universe universe;
-	private Domain subject;
+	private DomainReference subject;
 	
 	private List<ExpressionAST> filters = new ArrayList<>();
 	private List<ExpressionAST> facets = new ArrayList<>();
 	private List<ExpressionAST> metrics = new ArrayList<>();
 	
-	public QueryExpression(Universe universe, Domain subject) {
-		this.universe = universe;
+	public QueryExpression(DomainReference subject) {
 		this.subject = subject;
 	}
 	
 	public DomainReference getSubject() {
-		return new DomainReference(universe, subject);
+		return subject;
 	}
 	
 	public List<ExpressionAST> getFilters() {
@@ -80,7 +74,7 @@ public class QueryExpression implements ExpressionLeaf {
 	public IDomain getImageDomain() {
 		if (facets.isEmpty() && metrics.isEmpty()) {
 			// if no need to groupBy, the query is the same as the original domain
-			return new ProxyDomainDomain(universe, subject);
+			return subject.getImageDomain();
 		} else {
 			// else it's a new kind of object. Maybe we will need to create a QueryDomain at some point.
 			return IDomain.OBJECT;
@@ -89,12 +83,12 @@ public class QueryExpression implements ExpressionLeaf {
 
 	@Override
 	public IDomain getSourceDomain() {
-		return new ProxyDomainDomain(universe, subject);
+		return subject.getSourceDomain();
 	}
 
 	@Override
 	public String prettyPrint() {
-		StringBuffer print = new StringBuffer((new DomainReference(universe, subject)).prettyPrint());
+		StringBuffer print = new StringBuffer(subject.prettyPrint());
 		for (ExpressionAST filter : filters) {
 			print.append(" FILTER ").append(filter.prettyPrint());
 		}
