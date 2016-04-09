4043c6fd4e8a0c363e27ab93c629c8cb96d0a1c2
Serge Fantino <serge@squidsolutions.com>
2016-04-09 16:17:29 +0200

    cleanup

--- src/main/java/com/squid/kraken/v4/api/core/projectanalysisjob/AnalysisJobComputer.java
+++ src/main/java/com/squid/kraken/v4/api/core/projectanalysisjob/AnalysisJobComputer.java
@@ -57,7 +57,6 @@ import com.squid.kraken.v4.core.analysis.universe.Space;
 import com.squid.kraken.v4.core.analysis.universe.Universe;
 import com.squid.kraken.v4.core.expression.visitor.ExtractReferences;
 import com.squid.kraken.v4.export.ExportSourceWriter;
-import com.squid.kraken.v4.export.ExportSourceWriterVelocity;
 import com.squid.kraken.v4.model.AccessRight.Role;
 import com.squid.kraken.v4.model.DataTable;
 import com.squid.kraken.v4.model.Dimension;
