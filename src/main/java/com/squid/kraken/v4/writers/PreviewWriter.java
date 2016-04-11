83ff71b7a40ddad79e242fe4e531853263987cd3
Serge Fantino <serge@squidsolutions.com>
2016-04-11 11:07:16 +0200

    cosmetic

--- src/main/java/com/squid/kraken/v4/writers/PreviewWriter.java
+++ src/main/java/com/squid/kraken/v4/writers/PreviewWriter.java
@@ -8,24 +8,26 @@ import com.squid.kraken.v4.core.analysis.datamatrix.DataMatrix;
 
 public class PreviewWriter extends QueryWriter {
 
-	private DataMatrix dm ; 
-	
-	@Override 
-	public void write() throws ScopeException{
-		if (val !=null){
-			if (val instanceof RawMatrix){
-				this.dm= new DataMatrix( db,  (RawMatrix)val, mapper.getMeasureMapping(), mapper.getAxisMapping());
-			}else{
-				if  (val instanceof RedisCacheValuesList){
+	private DataMatrix dm = null;
+
+	public PreviewWriter() {
+	}
+
+	@Override
+	public void write() throws ScopeException {
+		if (val != null) {
+			if (val instanceof RawMatrix) {
+				this.dm = new DataMatrix(db, (RawMatrix) val, mapper.getMeasureMapping(), mapper.getAxisMapping());
+			} else {
+				if (val instanceof RedisCacheValuesList) {
 					RawMatrix raw = RedisCacheManager.getInstance().getRawMatrix(val.getRedisKey());
-					this.dm = new DataMatrix( db,  raw, mapper.getMeasureMapping(), mapper.getAxisMapping());				
+					this.dm = new DataMatrix(db, raw, mapper.getMeasureMapping(), mapper.getAxisMapping());
 				}
 			}
 		}
 	}
-	
-	
-	public DataMatrix getDataMatrix(){
+
+	public DataMatrix getDataMatrix() {
 		return dm;
 	};
 	
