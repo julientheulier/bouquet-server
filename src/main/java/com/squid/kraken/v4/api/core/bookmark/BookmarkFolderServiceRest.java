93c6d24db8571ca16f25117727c7e1ec47699c63
olivier <obalbous@squidsolutions.com>
2016-06-28 14:12:38 +0200

    T1483 /bookmarkfolders endpoint should return a list of bookmarkfolders

--- src/main/java/com/squid/kraken/v4/api/core/bookmark/BookmarkFolderServiceRest.java
+++ src/main/java/com/squid/kraken/v4/api/core/bookmark/BookmarkFolderServiceRest.java
@@ -59,8 +59,8 @@ public class BookmarkFolderServiceRest extends BaseServiceRest {
 	@GET
 	@Path("")
 	@ApiOperation(value = "Get My Bookmarks")
-	public BookmarkFolder read() {
-		return delegate.read(userContext, null);
+	public List<BookmarkFolder> read() {
+		return delegate.readFolders(userContext, null);
 	}
 
 	@GET
