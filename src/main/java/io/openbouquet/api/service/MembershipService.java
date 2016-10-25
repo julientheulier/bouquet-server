3fc77ec3a696f56b6344b80773dc102763146ffa
Serge Fantino <serge@squidsolutions.com>
2016-10-25 14:56:47 +0200

    T2102: updated the membership API + adding @produces

--- src/main/java/io/openbouquet/api/service/MembershipService.java
+++ src/main/java/io/openbouquet/api/service/MembershipService.java
@@ -2,6 +2,7 @@ package io.openbouquet.api.service;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
@@ -12,14 +13,17 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import com.squid.enterprise.model.Invitation;
+
 import io.openbouquet.api.model.Membership;
 import io.openbouquet.api.model.User;
 
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public interface MembershipService {
 
     @GET
-    @Path("/teamMembership/{teamid}")
+    @Path("/teamMembership")
     public Membership get(@HeaderParam("Authorization") String authorization, @PathParam("teamid") String teamid);
 
     @GET
@@ -34,12 +38,7 @@ public interface MembershipService {
     @POST
     @Path("/team/{teamid}/invite")
     public String inviteMember(@HeaderParam("Authorization") String authorization, @PathParam("teamid") String teamid,
-            @QueryParam("email") String email, String data);
-
-    @POST
-    @Path("/team/{teamid}/invite/{userid}")
-    public String inviteMember(@HeaderParam("Authorization") String authorization, @PathParam("teamid") String teamid,
-            @PathParam("userid") Long userId, String data);
+            @QueryParam("email") String email, Invitation invitation);
 
     @DELETE
     @Path("/memberships/{teamid}/member/{userid}")
