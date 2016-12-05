8cc2aa7305fcb3f4abf82c4fec0dfc7d2166b3e7
Serge Fantino <serge@squidsolutions.com>
2016-12-05 11:11:08 +0100

    resolving conflict before cherry picking

--- src/main/java/com/squid/kraken/v4/api/core/analytics/AnalyticsServiceBaseImpl.java
+++ src/main/java/com/squid/kraken/v4/api/core/analytics/AnalyticsServiceBaseImpl.java
@@ -93,7 +93,6 @@ import com.squid.kraken.v4.api.core.JobStats;
 import com.squid.kraken.v4.api.core.ObjectNotFoundAPIException;
 import com.squid.kraken.v4.api.core.PerfDB;
 import com.squid.kraken.v4.api.core.bookmark.BookmarkServiceBaseImpl;
-import com.squid.kraken.v4.api.core.customer.StateServiceBaseImpl;
 import com.squid.kraken.v4.api.core.project.ProjectServiceBaseImpl;
 import com.squid.kraken.v4.api.core.projectanalysisjob.AnalysisJobComputer;
 import com.squid.kraken.v4.caching.NotInCacheException;
