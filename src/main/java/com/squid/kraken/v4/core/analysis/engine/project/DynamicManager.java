f8f6cbe021c6292187a5026dcb7f2772a6effdc9
Serge Fantino <serge@squidsolutions.com>
2017-06-01 14:13:56 +0200

    change naming scheme when using FK: use the column name instead

--- src/main/java/com/squid/kraken/v4/core/analysis/engine/project/DynamicManager.java
+++ src/main/java/com/squid/kraken/v4/core/analysis/engine/project/DynamicManager.java
@@ -389,13 +389,22 @@ public class DynamicManager {
 								String rightName = target.getName();
 								samesame = dedupLinks.get(link);
 								if (samesame != null) {
-									leftName += "[" + fk.getName() + "]";
-									rightName += "[" + fk.getName() + "]";
+									for (KeyPair key : fk.getKeys()) {
+										leftName += " "+key.getPrimary().getName();
+										rightName += " "+key.getExported().getName();
+									}
+									//leftName += "[" + fk.getName() + "]";
+									//rightName += "[" + fk.getName() + "]";
 									if (samesame.first) {// it's the first
-										samesame.rel.setLeftName(
-												samesame.rel.getLeftName() + "[" + samesame.fk.getName() + "]");
-										samesame.rel.setRightName(
-												samesame.rel.getRightName() + "[" + samesame.fk.getName() + "]");
+										String samesameLeftName = samesame.rel.getLeftName();
+										String samesameRightName = samesame.rel.getRightName();
+										for (KeyPair key : samesame.fk.getKeys()) {
+											samesameLeftName += " "+key.getPrimary().getName();
+											samesameRightName += " "+key.getExported().getName();
+										}
+										samesame.rel.setLeftName(samesameLeftName);
+										samesame.rel.setRightName(samesameRightName);
+										samesame.first = false;// update only once
 									}
 								}
 								RelationPK relationPk = new RelationPK(project.getId(), digest);
