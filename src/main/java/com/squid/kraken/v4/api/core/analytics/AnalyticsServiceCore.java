428150bb19675db30b2e3d661d6a8092b231bdf5
Julien Theulier <julien@squidsolutions.com>
2017-09-29 16:37:32 +0200

    T3190 fix regression with __segment not in scope

--- src/main/java/com/squid/kraken/v4/api/core/analytics/AnalyticsServiceCore.java
+++ src/main/java/com/squid/kraken/v4/api/core/analytics/AnalyticsServiceCore.java
@@ -594,8 +594,7 @@ public class AnalyticsServiceCore {
 				// look for the selection
 				for (Facet facet : selection.getFacets()) {
 					if (!facet.getSelectedItems().isEmpty()) {
-						ExpressionAST exprFacet = globalScope.parseExpression(facet.getId());
-						if (exprFacet.prettyPrint(new PrettyPrintOptions(ReferenceStyle.IDENTIFIER, null)).equals(period)){
+						if (!SegmentManager.isSegmentFacet(facet) && globalScope.parseExpression(facet.getId()).prettyPrint(new PrettyPrintOptions(ReferenceStyle.IDENTIFIER, null)).equals(period)){
 							// it's the period
 							List<FacetMember> items = facet.getSelectedItems();
 							if (items.size()==1) {
